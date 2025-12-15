4eb6fdc77f42a05f71ba9a347c29365a1e55cc4f
Khushi <khushi@synapseco.com>
2025-12-15 14:05:40 +0530

    New commit

--- Schema/User.js
+++ Schema/User.js
@@ -1,13 +1,13 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema=mongoose.Schema({
-    firstname:{type:String,reruired:true},
-    lastname:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true,unique:true},
-    secret:{type:String,required:true,unique:true}
-})
+const UserSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  secret: { type: String, required: true, unique: true },
+});
 
-const User=mongoose.model("User",UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-export default User;
+module.exports = User;
