85975e828e63d975703e9ee7cb4f634904c90a79
Khushi <khushi@synapseco.com>
2025-12-16 17:26:19 +0530

    added domain

--- otpgenerate/otpgen.js
+++ otpgenerate/otpgen.js
@@ -4,13 +4,6 @@ const fs = require("fs");
 
 async function otpGen(email) {
   const secret = authenticator.generateSecret();
-
-  
-    
-    
-    console.log("Current Token:", secret, "| Time:", new Date().toLocaleTimeString());
-    
-
   const otpauthURL = authenticator.keyuri(email, "MyApp", secret);
   const qrCodeDataURL=await QRCode.toDataURL(otpauthURL)
   
