86e3290dab87b5edbb1889e114d17b1a12782884
Khushi <khushi@synapseco.com>
2025-12-19 16:29:22 +0530

    auth changes

--- controllers/middleware/check_permission.js
+++ controllers/middleware/check_permission.js
@@ -4,7 +4,7 @@ exports.PermissionCheck=(userPermission)=>{
     return async(req,res,next)=>{
     try{
         const userId=req.user._id
-       const user= await User.findById(userId).populate({path:"role",populate:{path:"permissions"}})
+       const user= await User.findById(userId).populate({path:"role",populate:{path:"permissions",select:"name"}})
        const hasPermission=user.role.permissions.some(perm=>perm.name===userPermission)
        if(hasPermission){
         next()
