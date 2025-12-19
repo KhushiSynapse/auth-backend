4ce6470c277eeb07772ad7c22074e4432d6a5f32
Khushi <khushi@synapseco.com>
2025-12-19 17:23:17 +0530

    auth changes

--- Seed/AttachPermission.js
+++ Seed/AttachPermission.js
@@ -5,7 +5,7 @@ async function AttachPermissions(){
     const createUser=await Permission.findOne({name:"create-user"})
     const deleteUser=await Permission.findOne({name:"delete-user"})
     const updateUser=await Permission.findOne({name:"update-user"})
-    const listUser=await Permission.findOne({name:"list-user"})
+    const listUsers=await Permission.findOne({name:"list-user"})
     const viewProfile=await Permission.findOne({name:"view-profile"})
     const manageRole=await Permission.findOne({name:"manage-roles"})
 
@@ -16,7 +16,7 @@ async function AttachPermissions(){
                 permissions:[createUser._id,
                     deleteUser._id,
                     updateUser._id,
-                    listUser._id,
+                    listUsers._id,
                     viewProfile._id,
                     manageRole._id
 
@@ -29,7 +29,7 @@ async function AttachPermissions(){
              await Role.create({name:"manager",
                 permissions:[
                     
-                    listUser._id,
+                    listUsers._id,
                     viewProfile._id,
                   
 
