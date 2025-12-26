33de7dc21d7ba17557f411210927e032b01e86eb
Khushi <khushi@synapseco.com>
2025-12-26 12:06:39 +0530

    lang middleware added

--- controllers/middleware/check_language.js
+++ controllers/middleware/check_language.js
@@ -1,5 +1,7 @@
 
 exports.checkLanguage=async(req,res,next)=>{
+      console.log("Accept-Language header received:", req.headers["Accept-Language"]);
+
     req.lang=req.headers["Accept-Language"]||"en"
     next()
 }
