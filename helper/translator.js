61a991528482a0fa13f1913efa0bd1b1d0794505
Khushi <khushi@synapseco.com>
2025-12-26 11:14:50 +0530

    lang middleware added

--- helper/translator.js
+++ helper/translator.js
@@ -1,6 +1,12 @@
-const en=require("../../configure/locale/en.json")
-const ar=require("../../configure/locale/ar.json")
-const fr=require("../../configure/locale/fr.json")
-const sp=require("../../configure/locale/sp.json")
+const en=require("../locale/en.json")
+const ar=require("../locale/ar.json")
+const fr=require("../locale/fr.json")
+const sp=require("../locale/sp.json")
 
 const translations={en,sp,fr,ar}
+
+function t(key,lang="en"){
+    return translations[lang]?.key||translations.en[key]
+}
+
+module.exports=t
