d6432c019596bb4a54ed013c87bc94b9c7575d02
Khushi <khushi@synapseco.com>
2025-12-29 10:16:34 +0530

    Permission added

--- Schema/Product.js
+++ Schema/Product.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const ProductSchema=new mongoose.Schema({
     name:{type:"String",required:true},
     price:{type:"Number",required:true},
+    category:{type:"String",required:true},
     desc:{type:"String",required:true},
     imageURL:{type:"String",required:true},
     createdAt:{type:"Date",default:Date.now}
