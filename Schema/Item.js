76d93d89ff43250a9ff9e56f7639750f7ca64a28
Khushi <khushi@synapseco.com>
2025-12-29 12:40:51 +0530

    get-cartProducts api added

--- Schema/Item.js
+++ Schema/Item.js
@@ -5,7 +5,7 @@ const CartSchema=new mongoose.Schema({
     price:{type:"Number",required:true},
     imageURL:{type:"String",required:true},
     addedAt:{type:"Date",default:Date.now},
-    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User"}
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true}
 })
 
 const Item=mongoose.model("Item",CartSchema)
