d94928e1da94a7476d43d650890769efba1c8133
Khushi <khushi@synapseco.com>
2025-12-29 11:57:11 +0530

    save-product api added

--- Schema/Item.js
+++ Schema/Item.js
@@ -3,7 +3,6 @@ const mongoose=require("mongoose")
 const CartSchema=new mongoose.Schema({
     name:{type:"String",required:true},
     price:{type:"Number",required:true},
-    category:{type:"String",required:true},
     imageURL:{type:"String",required:true},
     addedAt:{type:"Date",default:Date.now}
 })
