7f56f6aefbf1ee51c48bf05a4ce2b78f20ee5265
Khushi <khushi@synapseco.com>
2026-01-02 11:04:13 +0530

    schema changed

--- Schema/Item.js
+++ Schema/Item.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const CartSchema=new mongoose.Schema({
     name:{type:"String",required:true},
     price:{type:"Number",required:true},
-    imageURL:{type:"String",required:true},
+    imageURL:{type:["String"],required:true},
     addedAt:{type:"Date",default:Date.now},
     Quantity:{type:"Number",default:1},
     userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true}
