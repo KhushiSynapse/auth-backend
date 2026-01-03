7e7fd6acbb1c85417d8cb77f88aad1f7f960012f
Khushi <khushi@synapseco.com>
2026-01-03 12:01:13 +0530

    Paypal

--- controllers/mail_auth.js
+++ controllers/mail_auth.js
@@ -14,7 +14,7 @@ const Item=require("../Schema/Item")
 const paypal = require("@paypal/paypal-server-sdk");
 
 
-const paypalClient=new paypal.core.SandboxEnvironment(
+const environment=new paypal.core.SandboxEnvironment(
     process.env.PAYPAL_CLIENT_ID,
         process.env.PAYPAL_CLIENT_SECRET
     
