53c516a07c4aca3ae7de5db3e599f623f8676b7f
Khushi <khushi@synapseco.com>
2026-01-05 15:33:03 +0530

    order api

--- Schema/OrderItem.js
+++ Schema/OrderItem.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const OrderItemSchema=new mongoose.Schema({
-    orderid:{type:mongoose.Schema.Types.ObjectId,required:true},
+    orderid:{type:mongoose.Schema.Types.ObjectId,ref:"Order",required:true},
     name:{type:"String",required:true},
         price:{type:"Number",required:true},
         addedAt:{type:"Date",default:Date.now},
