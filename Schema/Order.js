649d6dd6bffd68b5cb9df8cefd702e8bc08b4b08
Khushi <khushi@synapseco.com>
2026-01-05 12:15:23 +0530

    order api

--- Schema/Order.js
+++ Schema/Order.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const OrderSchema = new mongoose.Schema({
     id:{type:"String",required:true},
     userid:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
-    amount:{type:"Number",required:true},
+    amount:{type:"String",required:true},
     paymentstatus:{type:"String",required:true},
     orderstatus:{type:"String",required:true},
     currency:{type:"String"},
