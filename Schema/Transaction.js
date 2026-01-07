a9f7a00ec7f8e1a06a3b0f0e739f2beddddd7246
Khushi <khushi@synapseco.com>
2026-01-07 13:25:49 +0530

    refund

--- Schema/Transaction.js
+++ Schema/Transaction.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose")
 
 const TransactionSchema=new mongoose.Schema({
-    userId:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
     orderId:{type:mongoose.Schema.Types.ObjectId,ref:"Order",required:true},
     paymentMethod:{type:String,default:"PayPal"},
     amount:{type:String,required:true},
