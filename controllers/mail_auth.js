918dab76cb8a948b3362f62783cf2a1d033d6fbc
Khushi <khushi@synapseco.com>
2026-01-09 17:04:29 +0530

    websocket

--- controllers/mail_auth.js
+++ controllers/mail_auth.js
@@ -774,3 +774,18 @@ exports.getTransactionDetails=async(req,res)=>{
         return res.status(500).json({message:error.message})
     }
 }
+
+exports.getSearchItem=async(req,res)=>{
+    try{
+        const serach=req.params.serach
+        const response=await OrderItem.find({name:serach})
+        if(response.length>0){
+            return res.status(200).json(response)
+        }
+        else{
+            return res.status(204).json({message:"No item found"})
+        }
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
